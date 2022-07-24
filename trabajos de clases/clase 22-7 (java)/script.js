7ff4190708a4bd0b3ecdbcb54295d27d28ace5b2
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-07-24 16:30:24 -0300

    tarea 22-7

--- trabajos de clases/clase 22-7 (java)/script.js
+++ trabajos de clases/clase 22-7 (java)/script.js
@@ -1,1 +1,16 @@
-alert("bienvenido");
+let btn_nombre = document.getElementById("btn_nombre");
+btn_nombre.addEventListener("click", mostrar);
+
+function mostrar() {
+    let nombre = "pepe";
+    let apellido = "pepito";
+    let edad = "19";
+    let inputApellido = document.getElementById("inputApellido");
+    let inputNombre = document.getElementById("inputNombre");
+    let inputedad = document.getElementById("inputedad");
+    nombre = inputNombre.value;
+    apellido = inputApellido.value;
+    edad = inputedad.value;
+
+    alert("Bienvenido " + nombre +" "+ apellido +" "+ edad );
+}
