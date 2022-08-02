3a7528c47712643c961780cdf5d4ec35275ff9a1
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-08-02 20:12:09 -0300

    ejercicio de suma y resta de click

--- trabajos de clases/clase 26-7 (js)/script.js
+++ trabajos de clases/clase 26-7 (js)/script.js
@@ -3,21 +3,27 @@ let contador = 0;
 let contar = document.getElementById("contar");
 contar.addEventListener("click",contar1);
 
-let mostrar1 = document.getElementById("mostrar");
-mostrar1.addEventListener("click",mostarPantalla);
+let restar = document.getElementById("restar");
+contar.addEventListener("click",restar);
+
+//let mostrar1 = document.getElementById("mostrar");
+//mostrar1.addEventListener("click",mostarPantalla);
 
 function contar1(){
     contador++
-    console.log(contador);
+    document.querySelector("h1").innerHTML = contador;
 }
 
-function mostarPantalla(){
-    alert(contador);
+function restar(){
+    contador--;
+    document.querySelector("h1").innerHTML = contador;
 }
 
-let datos = document.getElementById("datos");
-datos.addEventListener("input", mostrar);
 
-function mostrar(){
-    console.log(datos.value)
-}
+
+//let datos = document.getElementById("datos");
+//datos.addEventListener("input", mostrar);
+
+//function mostrar(){
+    //console.log(datos.value)
+//}
