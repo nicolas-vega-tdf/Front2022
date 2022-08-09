b1cfed81b629d7a16b711a1bc9aa81794c763804
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-08-09 20:05:01 -0300

    sin terminar

--- practica/ejercicio n2/script.js
+++ practica/ejercicio n2/script.js
@@ -1,13 +1,15 @@
+ "use stric" 
 
+ let numeritos = 0;
 
 let boton = document.getElementById("boton");
 boton.addEventListener("click", mostrar);
 
 
 function mostrar(){
-    let numeritos = 0;
+    
     for(let i=0;i<100;i++){
-        numeritos++
+        numeritos =Math.floor((Math.random))
     }
     document.querySelector("h1").innerHTML = numeritos;
 }
