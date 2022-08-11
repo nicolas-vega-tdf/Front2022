13779955142efb597289a3d0c06de2d241370ba0
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-08-11 17:20:43 -0300

    ejercicio n del 1-100  terminado

--- practica/ejercicio n2/script.js
+++ practica/ejercicio n2/script.js
@@ -1,15 +1,13 @@
- "use stric" 
-
- let numeritos = 0;
 
 let boton = document.getElementById("boton");
 boton.addEventListener("click", mostrar);
 
 
 function mostrar(){
-    
+    let numero=0;
     for(let i=0;i<100;i++){
-        numeritos =Math.floor((Math.random))
+        numero =Math.floor((Math.random()*(100 - 1 + 1))+1);
     }
-    document.querySelector("h1").innerHTML = numeritos;
+    document.querySelector("h1").innerHTML = numero;
+    numeritos=0;
 }
