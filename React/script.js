2387c1936798b6637c784e1fd6f65657780eac8b
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2022-12-05 21:04:00 -0300

    correccion tarea rick and morty

--- React/script.js
+++ React/script.js
@@ -1,43 +1,27 @@
-let nombre = document.getElementById("nombre");
-let statu = document.getElementById("status");
-let foto = document.getElementById("foto");
 let btn = document.getElementById("btn");
 btn.addEventListener("click",toggle);
 
 function toggle(){
-    document.querySelector("body").classList.toggle("dark")
+    document.querySelector("body").classList.toggle("dark");
+    document.querySelector("button").classList.toggle("dark");
 }
 
-
-async function obtenerNombre(){
+async function obtenerDatos(){
     console.log("Buscando Nombre");
     let res =  await fetch("https://rickandmortyapi.com/api/character");
     let data = await res.json();
-   data.results.forEach(element => {
-    nombre.innerHTML += element.name;
+    console.log(data);
+    data.results.forEach(element => {
+        document.getElementById('table').innerHTML+=`  <tr>
+                                                            <td>${element.name}</td>
+                                                            <td>${element.status}</td>
+                                                            <td>${element.image}</td>
+                                                        </tr>`
    });
 }
 
-async function obtenerEspecie(){
-    console.log("Buscando especie");
-    let res =  await fetch("https://rickandmortyapi.com/api/character");
-    let data = await res.json();
-    data.results.forEach(element => {
-        statu.innerHTML += element.species;
-    });
-}
 
-async function obtenerImagen(){
-    console.log("Buscando imagen");
-    let res =  await fetch("https://rickandmortyapi.com/api/character");
-    let data = await res.json();
-    data.results.forEach(element => {
-       foto.innerHTML += element.image
-    });
-}
-//data.results.forEach(element => {
-//    console.log(element.name);
-//});
-obtenerNombre()
-obtenerEspecie()
-obtenerImagen()
+obtenerDatos()
+
+
+
